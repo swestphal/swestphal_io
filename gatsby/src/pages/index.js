5d0a98e9dca5720c6a21d1a120ab5f3db79b78cb
Simone-Westphal <sw@thinkweb360.de>
2020-11-01 17:44:37 +0100

    first commit

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <>
+            <p>Hey! I'm a page</p>
+            <p>Hey I'm another element</p>
+        </>
+    );
+}
