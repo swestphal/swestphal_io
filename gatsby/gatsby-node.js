5ebbb66b499af8a6f011658837f0b79385883eed
Simone-Westphal <sw@thinkweb360.de>
2020-11-12 20:32:33 +0100

    feature: generate pages for project categories depending on count

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,5 +1,46 @@
 import path from 'path';
 
+function countProjectsInCategories(projects) {
+  // Return the projects with counts
+
+  const counts = projects
+    .map((project) => project.relatedCategories)
+    .flat()
+    .reduce((acc, category) => {
+      // check if this is an existing category
+      const existingCategory = acc[category.classification.id];
+      if (existingCategory) {
+        //  if it is, increment by 1
+        existingCategory.count += 1;
+      } else {
+        // otherwise create a new entry in our acc and set it to one
+        acc[category.classification.id] = {
+          id: category.classification.id,
+          name: category.classification.name,
+          colour: category.classification.colour,
+          slug: category.classification.slug.current,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+  counts[0] = {
+    id: 0,
+    name: 'All',
+    colour: 'none',
+    slug: 'projects',
+    count: 10,
+  };
+  // sort them based on their count
+  console.log(counts);
+  const sortedCategories = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+
+  console.log(sortedCategories);
+  return sortedCategories;
+}
+
 async function turnProjectCategoriesIntoPages({ graphql, actions }) {
   // 1. Get the template
   const categoryTemplate = path.resolve('./src/pages/projects.js');
@@ -17,19 +58,32 @@ async function turnProjectCategoriesIntoPages({ graphql, actions }) {
           }
         }
       }
+      projects: allSanityProject {
+        nodes {
+          relatedCategories {
+            classification {
+              id
+              name
+              colour
+              slug {
+                current
+              }
+            }
+          }
+        }
+      }
     }
   `);
   // get count of pages of that category
   const pageSize = parseInt(process.env.GATSBY_PROJECT_SIZE);
-  const pageCount = Math.ceil(data.categories.totalCount / pageSize);
 
-  data.categories.nodes.forEach((category) => {
-    Array.from({ length: pageCount }).forEach((_, i) => {
-      console.log(`Creating page for category`, category.name);
-      console.log(`Page`, i);
-      console.log(data.categories);
+  const categoriesWithCounts = countProjectsInCategories(data.projects.nodes);
+  categoriesWithCounts.forEach((category) => {
+    console.log(category);
+    const pageCount = Math.ceil(category.count / pageSize);
+    Array.from({ length: pageCount }).forEach((a, i) => {
       actions.createPage({
-        path: `projects/${category.slug.current}/${i + 1}`,
+        path: `projects/${category.slug}/${i + 1}`,
         component: categoryTemplate,
         context: {
           skip: i * pageSize,
@@ -44,17 +98,17 @@ async function turnProjectCategoriesIntoPages({ graphql, actions }) {
   });
   // 3. createPage for that category
   /* data.categories.nodes.forEach((category) => {
-    console.log(`Creating page for category`, category.name);
-    actions.createPage({
-      path: `category/${category.name}`,
-      component: categoryTemplate,
-      context: {
-        category: category.name,
-        //  Regex for Topping
-        // toppingRegex: `/${topping.name}/i`,
-      },
-    });
-  }); */
+      console.log(`Creating page for category`, category.name);
+      actions.createPage({
+        path: `category/${category.name}`,
+        component: categoryTemplate,
+        context: {
+          category: category.name,
+          //  Regex for Topping
+          // toppingRegex: `/${topping.name}/i`,
+        },
+      });
+    }); */
   // 4. Pass category data to project.js
 }
 
