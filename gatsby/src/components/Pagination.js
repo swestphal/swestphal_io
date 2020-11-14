fd0c8c1201158698e79cc61f51d1e4ac3b7f4edd
Simone-Westphal <simone.westphal@me.com>
2020-11-14 10:38:36 +0100

    finished Pagination

--- gatsby/src/components/Pagination.js
+++ gatsby/src/components/Pagination.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 export default function Pagination({
-  perPage,
+  pageSize,
   totalCount,
   currentPage,
   skip,
@@ -10,21 +10,48 @@ export default function Pagination({
   categorySlug,
   categoryName,
 }) {
-  console.log(categoryName);
-  console.log(categorySlug);
-  const totalPages = Math.ceil(totalCount / perPage);
+  const totalPages = Math.ceil(totalCount / pageSize);
   const prevPage = currentPage - 1;
   const nextPage = currentPage + 1;
-
+  const hasNextPage = nextPage <= totalCount;
+  const hasPrevPage = prevPage > 0;
   const categoryPath = categoryName !== 'All' ? `${categorySlug}` : ``;
 
   const linkToPrev = currentPage > 2 ? `${currentPage - 1}` : '';
-  const linkToNext = currentPage > 0 ? `${currentPage + 1}` : ``;
+  const linkToNext = currentPage < totalPages ? `${currentPage + 1}` : ``;
 
   return (
-    <div>
-      <Link to={`${base}${categoryPath}/${linkToPrev}`}>Prev</Link>
-      <Link to={`${base}${categoryPath}/${linkToNext}`}>Next</Link>
+    <div className="pagination">
+      <ul>
+        <li>
+          <Link
+            disabled={!hasPrevPage}
+            to={`${base}${categoryPath}/${linkToPrev}`}
+          >
+            Prev
+          </Link>
+        </li>
+
+        {Array.from({ length: totalCount }).map((_, i) => (
+          <li>
+            <Link
+              className={currentPage === 1 && i === 0 ? 'active' : ''}
+              to={`${base}${categorySlug}/${i === 0 ? '' : i + 1}`}
+            >
+              {i + 1}
+            </Link>
+          </li>
+        ))}
+
+        <li>
+          <Link
+            disabled={!hasNextPage}
+            to={`${base}${categoryPath}/${linkToNext}`}
+          >
+            Next
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
