c74b8ae5edabc6bcc1de5ccf971464bc40eb18e3
Simone Westphal <sw@simonewestphal.de>
2020-12-22 17:01:13 +0100

    refactor: Remove sticky header and replace with css solution

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 import { bool } from 'prop-types';
 import { AiFillPhone, AiFillMail, AiFillCopyrightCircle } from 'react-icons/ai';
-import Sticky from 'wil-react-sticky';
+// import Sticky from 'wil-react-sticky';
 // import Logo from '../assets/images/simonewestphal.png';
 //  <div className="sidebar__logo">
 //               <img src={Logo} alt="Logo" />
@@ -21,67 +21,62 @@ export default function Nav({ location }) {
   return (
     <>
       <StyledSidebar id="sidebar">
-        <Sticky
-          containerSelectorFocus="#container"
-          stickyEnableRange={[980, Infinity]}
-        >
-          <div id="sidebar__inner">
-            <Burger open={open} setOpen={setOpen} />
+        <div id="sidebar__inner">
+          <Burger open={open} setOpen={setOpen} />
 
-            <div className="sidebar__infos">
-              <a
-                href="mailto:hello@swestphal.io"
-                className="link infos__icons--mail"
-              >
-                <AiFillMail />
-                hello@swestphal.io
-              </a>
-              <a href="tel:4935873159989" className="link infos__icons--phone">
-                <AiFillPhone />
-                +49 35873 / 15 99 89
-              </a>
-            </div>
-            <StyledNav className="sidebar__menu" open={open}>
-              <ul className="menu">
-                <li onClick={() => setOpen(!open)}>
-                  <Link to="/">
-                    <span>Home</span>
-                  </Link>
-                </li>
-                <li onClick={() => setOpen(!open)}>
-                  <Link to="/blog/">
-                    <span>Blog</span>
-                  </Link>
-                </li>
-                <li onClick={() => setOpen(!open)}>
-                  <Link to="/services/">
-                    <span>Services</span>
-                  </Link>
-                </li>
-                <li onClick={() => setOpen(!open)}>
-                  <Link
-                    to="/projekte/"
-                    className={isParentPage('projekt') ? 'active' : ''}
-                  >
-                    <span>Projekte</span>
-                  </Link>
-                </li>
-                <li onClick={() => setOpen(!open)}>
-                  <Link to="/kontakt/">
-                    <span>Kontakt</span>
-                  </Link>
-                </li>
-              </ul>
-            </StyledNav>
-
-            <div className="sidebar__infos">
-              <p className="infos__copyright">
-                <AiFillCopyrightCircle /> {new Date().getFullYear()} Simone
-                Westphal
-              </p>
-            </div>
+          <div className="sidebar__infos">
+            <a
+              href="mailto:hello@swestphal.io"
+              className="link infos__icons--mail"
+            >
+              <AiFillMail />
+              hello@swestphal.io
+            </a>
+            <a href="tel:4935873159989" className="link infos__icons--phone">
+              <AiFillPhone />
+              +49 35873 / 15 99 89
+            </a>
           </div>
-        </Sticky>
+          <StyledNav className="sidebar__menu" open={open}>
+            <ul className="menu">
+              <li onClick={() => setOpen(!open)}>
+                <Link to="/">
+                  <span>Home</span>
+                </Link>
+              </li>
+              <li onClick={() => setOpen(!open)}>
+                <Link to="/blog/">
+                  <span>Blog</span>
+                </Link>
+              </li>
+              <li onClick={() => setOpen(!open)}>
+                <Link to="/services/">
+                  <span>Services</span>
+                </Link>
+              </li>
+              <li onClick={() => setOpen(!open)}>
+                <Link
+                  to="/projekte/"
+                  className={isParentPage('projekt') ? 'active' : ''}
+                >
+                  <span>Projekte</span>
+                </Link>
+              </li>
+              <li onClick={() => setOpen(!open)}>
+                <Link to="/kontakt/">
+                  <span>Kontakt</span>
+                </Link>
+              </li>
+            </ul>
+          </StyledNav>
+
+          <div className="sidebar__infos">
+            <p className="infos__copyright">
+              <AiFillCopyrightCircle /> {new Date().getFullYear()} Simone
+              Westphal
+            </p>
+          </div>
+        </div>
       </StyledSidebar>
     </>
   );
